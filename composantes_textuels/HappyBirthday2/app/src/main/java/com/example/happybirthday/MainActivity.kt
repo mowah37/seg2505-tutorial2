d6596a6227a6b9f2edcfd49b3c592ac592be2f72
mowah37 <mwand011@uottawa.ca>
2024-10-16 22:39:39 -0400

    Modification de la taille du texte et utilisation de Row

--- composantes_textuels/HappyBirthday2/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ composantes_textuels/HappyBirthday2/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +26,7 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.fillMaxSize(),
                         color=MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(message = "Happy Birthday!")
+                    GreetingText(message = "Happy Birthday!",from="From Emma")
                 }
             }
         }
@@ -32,16 +34,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
-    Text(
-        text = message
-    )
+fun GreetingText(message: String, modifier: Modifier = Modifier,from:String) {
+ Row {
+     Row {
+         Text(
+             text = message,
+             fontSize = 30.sp,
+             lineHeight = 116.sp,
+         )
+     }
+     Text(
+         text = from,
+         fontSize = 36.sp
+     )
+ }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Sam!")
+        GreetingText(message = "Happy Birthday Sam!",from="From Emma")
     }
 }
